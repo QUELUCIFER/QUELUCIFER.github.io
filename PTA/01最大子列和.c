fd06ebd4e2f0e91c787f46d662339adbcd129b89
QUELUCIFER <43593120+QUELUCIFER@users.noreply.github.com>
2019-03-07 09:34:18 +0800

    Update and rename 01最大子列和.md to 01最大子列和.c

--- PTA/01最大子列和.c
+++ PTA/01最大子列和.c
@@ -32,3 +32,35 @@
 -2 11 -4 13 -5 -2
 输出样例:
 20
+
+
+#include<stdio.h>
+#define M 100001
+
+
+int MaxSubseqSum4( int A[], int N )
+{ 
+    int ThisSum, MaxSum;
+    int i;
+        ThisSum = MaxSum = 0;
+        for( i = 0; i < N; i++ ) {
+            ThisSum += A[i]; /* 向右累加 */
+            if( ThisSum > MaxSum )
+                MaxSum = ThisSum; /* 发现更大和则更新当前结果 */
+            else if( ThisSum < 0 ) /* 如果当前子列和为负 */
+                ThisSum = 0; /* 则不可能使后面的部分和增大，抛弃之 */
+}
+    return MaxSum;
+} 
+int main(void)
+{
+  int k,c[M];
+  scanf("%d",&k);
+  for(int i=0;i<k;i++)
+  {
+    scanf("%d",&c[i]);
+  }
+  int sum=MaxSubseqSum4(c,k);
+  printf("%d",sum);
+  return 0;
+}
