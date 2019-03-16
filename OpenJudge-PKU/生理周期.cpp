cbe8f4aaa7edbcbfc90ea9200821c9ff61d45947
QUELUCIFER <43593120+QUELUCIFER@users.noreply.github.com>
2019-03-16 09:22:39 +0800

    Update 生理周期.cpp

--- OpenJudge-PKU/生理周期.cpp
+++ OpenJudge-PKU/生理周期.cpp
@@ -6,14 +6,15 @@ using namespace std;
 int main()
 {
    int p,e,i,d,CaseNo=0;
-   while(cin>>p>>e>>i&&p!=-1){
-    CaseNo++;
-    int k;
-    for(k=d+1;(k-p)%23;k++)
-    for(;(k-e)%28;k+=23);
-    for(;(k-i)%33;k+=23*28);
-    cout<<"Case"<<CaseNo<<":the next triple peak occurs in"<<k-d<<"days"<<endl;
-    
+   
+   while(cin>>p>>e>>i>>d&&p!=-1){
+       CaseNo++;
+       int k;
+       for(k=d+1;(k-p)%23;k++);
+       for(;(k-e)%28;k+=23);
+       for(;(k-i)%33;k+=23*28);
+       cout<<"CaseNo"<<CaseNo<<"The triple peak occurs in"<<k-d<<"days"<<endl;
    }
+   
    return 0;
 }
